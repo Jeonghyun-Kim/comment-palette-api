d281d592c8e4300dd1f565c6c858d1dd15396214
Kay Kim <kimjh@bawi.org>
2020-06-09 21:13:00 +0900

    change ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,21 +5,15 @@ module.exports = {
       name: "palette-api",
       // pm2로 실행될 파일 경로
       script: "./build/server.js",
-      // 개발환경시 적용될 설정 지정
       env: {
-          "PORT": 8081,
-          "NODE_ENV": "development",
-      },
-      // 배포환경시 적용될 설정 지정
-      env_production: {
           "PORT": 8081,
           "NODE_ENV": "production",
       },
       watch: true,
-      instances: 0,
-      exec_mode: 'cluster',
-      wait_ready: true,
-      listen_timeout: 50000,
+      // instances: 0,
+      // exec_mode: 'cluster',
+      // wait_ready: true,
+      // listen_timeout: 50000,
     }
   ]
 };
